5716c1fa29d7ea292acb9c0f5c3396a4585f7554
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-12-15 21:35:27 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 
 export const metadata = {
   title: "Royal Service | Catálogo",
-  description: "Catálogo + carrito (sin pasarela de pago)."
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
