46a7f73d32015c92f266183e86c5bd384cdf927a
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-12-15 22:01:17 -0600

    Create publicConfig.ts

---
+++ lib/firebase/publicConfig.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_PUBLIC_CONFIG = {
+  apiKey: "AIzaSyANNPGwqGCI6EDP9ADBSRBtQ1K217kN5qQ",
+  authDomain: "royal-service-catalog.firebaseapp.com",
+  projectId: "royal-service-catalog",
+  storageBucket: "royal-service-catalog.firebasestorage.app",
+  messagingSenderId: "434184838318",
+  appId: "1:434184838318:web:4f2f0d0f697a9ea1d2213f",
+} as const;
